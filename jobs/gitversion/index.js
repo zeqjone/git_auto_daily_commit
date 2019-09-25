72899fd6a31e1eef34573df7f6d8df11ea5d69c5
朱二强 <zeq_jone@163.com>
2019-09-25 16:24:17 +0800

    feature: 基本功能已经完成， 下一步增加定时任务

--- jobs/gitversion/index.js
+++ jobs/gitversion/index.js
@@ -3,73 +3,88 @@ const {managerPath} = require('../../config')
 const shell = require('shelljs')
 const fs = require('fs')
 
-const gitPull = () => {
-  shell.cd(managerPath);
-  shell.exit('git checkout dev')
-  shell.exec('git pull -f origin dev:dev')
-}
+class GitVersionJob {
+  constructor () {
+    this.version = '';
+    this.autoCommit();
+  }
 
-const setPackage = async (str) => {
-  let packagePath = path.resolve(managerPath, 'package.json');
-  let wStream = fs.createWriteStream(packagePath, {flags: 'r+'});
-  wStream.write(str, function (err) {
-    if (err) {
-      console.error('write err', err);
-    }
-  })
-  wStream.on('close', () => {
-    console.info('write stream close')
-  })
-  wStream.on('error', (err) => {
-    console.error('write stream error', err)
-  })
-}
+  gitPull()  {
+    shell.cd(managerPath);
+    shell.exec('git checkout dev')
+    shell.exec('git pull -f origin dev:dev')
+  }
 
-const getStrPackage = async () => {
-  return new Promise((resolve, reject) => {
+  gitPush () {
+    shell.exec('git add .');
+    shell.exec(`git commit -m "${this.version}"`);
+    shell.exec('git push origin dev:dev');
+  }
+  async setPackage (str) {
     let packagePath = path.resolve(managerPath, 'package.json');
-    let rStream = fs.createReadStream(packagePath, {flags: 'r+'});
-    let fc = '';
-    rStream.on('end', function() {
-      let arr = fc.split(/\n/g);
-      for(let i in arr) {
-        let str = arr[i];
-        if (/"version":/.test(str)) {
-          let vNo = str.replace(/.*(v\d+\.\d+\.\d+).*/, '$1')
-          let arrVno = vNo.split('.');
-          let lastNoPlus = parseInt(arrVno.pop()) + 1;
-          arrVno.push(lastNoPlus);
-          str = str.replace(vNo, arrVno.join('.'));
-          arr.splice(i, 1, str);
-          break;
-        }
+    let wStream = fs.createWriteStream(packagePath, {flags: 'r+'});
+    wStream.write(str, function (err) {
+      if (err) {
+        console.error('write err', err);
       }
-      resolve(arr.join('\n'))
     })
-    rStream.on('data', function(data) {
-      console.log('data')
-      fc += data;
+    wStream.on('close', () => {
+      console.info('write stream close')
     })
-    rStream.on('error', function(err) {
-      console.log('error')
-      console.log(err)
-      reject(err)
+    wStream.on('error', (err) => {
+      console.error('write stream error', err)
     })
-  })
-}
-
-module.exports = {
-  autoCommit () {
-    if (!shell.which('git')) {
-      shell.echo('sorry, this script requires git')
-      shell.exit(1)
+  }
+  async getStrPackage () {
+    return new Promise((resolve, reject) => {
+      const ins = this;
+      let packagePath = path.resolve(managerPath, 'package.json');
+      let rStream = fs.createReadStream(packagePath, {flags: 'r+'});
+      let fc = '';
+      rStream.on('end', function() {
+        let arr = fc.split(/\n/g);
+        for(let i in arr) {
+          let str = arr[i];
+          if (/"version":/.test(str)) {
+            let vNo = str.replace(/.*(\d+\.\d+\.\d+).*/, '$1')
+            let arrVno = vNo.split('.');
+            let lastNoPlus = parseInt(arrVno.pop()) + 1;
+            arrVno.push(lastNoPlus);
+            str = str.replace(vNo, arrVno.join('.'));
+            arr.splice(i, 1, str);
+            ins.version = str;
+            break;
+          }
+        }
+        resolve(arr.join('\n'))
+      })
+      rStream.on('data', function(data) {
+        console.log('data')
+        fc += data;
+      })
+      rStream.on('error', function(err) {
+        console.log('error')
+        console.log(err)
+        reject(err)
+      })
+    })
+  }
+  async autoCommit () {
+    console.log('git pull begin')
+    try {
+      if (!shell.which('git')) {
+        shell.echo('sorry, this script requires git')
+        shell.exit(1)
+      }
+      console.log('git pull end')
+      this.gitPull();
+      let str = await this.getStrPackage();
+      await this.setPackage(str);
+      this.gitPush();
+    } catch (err) {
+      console.error(err)
     }
-    gitPull();
-    arrVersion.push(lastVersion);
-    let packageFile = fs.readFileSync(package);
-  },
-  async mkVersionChange() {
-    let str = await getStrPackage();
-    await setPackage(str)
   }
 }
+
+module.exports = new GitVersionJob()
--- jobs/gitversion/test.js
+++ jobs/gitversion/test.js
@@ -1,7 +1,11 @@
 const m = require('./index')
 console.log('process begin')
-
-m.mkVersionChange()
-.then(() => console.log('enddd'))
-.catch(err => console.error(err));
-console.log('process end')
+try {
+  m.autoCommit()
+  .then(() => console.log('enddd'))
+  .catch(err => console.error(err));
+  console.log('process end')
+}
+catch (err) {
+  console.error(err)
+}
