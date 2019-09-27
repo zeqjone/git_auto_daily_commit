fe9cd931e2a4fa51c57dd183b144c77181dc965f
zeq <erqiang.zhu@redcore.cn>
2019-09-27 12:53:46 +0800

    修改任务计划

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const moment = require('moment')
 const gitVersionJob = require('./jobs/gitversion')
 const logger = require('./lib/logger')
 
-let job = schedule.scheduleJob('*/2 * * * 1-5', function () {
+let job = schedule.scheduleJob('30 23 * * 1-5', function () {
 // let job = schedule.scheduleJob('*/2 * * * 1-5', function () {
   logger.info(`git version commit start: ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
   gitVersionJob.autoCommit()
