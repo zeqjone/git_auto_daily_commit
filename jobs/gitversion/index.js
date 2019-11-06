e3ecc9f055ef5ccdb5b4077b50eca818d8d5809b
朱二强 <zeq_jone@163.com>
2019-11-06 10:15:17 +0800

    fix: 解决 package.json 和 ui/package.json 版本号不一致的问题

--- jobs/gitversion/index.js
+++ jobs/gitversion/index.js
@@ -1,15 +1,15 @@
 const path = require('path')
-const {managerPath} = require('../../config')
+const { managerPath } = require('../../config')
 const shell = require('shelljs')
 const fs = require('fs')
 const logger = require('../../lib/logger')
 
 class GitVersionJob {
-  constructor () {
+  constructor() {
     this.version = '';
   }
 
-  gitPull()  {
+  gitPull() {
     logger.info('job gitVersion task 1: git pull begin')
     shell.cd(managerPath);
     shell.exec('git checkout dev')
@@ -17,45 +17,53 @@ class GitVersionJob {
     logger.info('job gitVersion task 1: git pull end')
   }
 
-  gitPush () {
+  gitPush() {
     logger.info('job gitVersion task 3: {{');
     shell.exec('git add .');
     shell.exec(`git commit -m "${this.version}"`);
     shell.exec('git push origin dev:dev');
     logger.info('job gitVersion task 3: }}');
   }
-  async makeVersionChange () {
+  async makeVersionChange() {
     logger.info('job gitVersion task 2: {{')
     let packagePath = path.resolve(managerPath, 'package.json');
-    let str = await this.getStrPackage(packagePath);
-    await this.setPackage(packagePath, str);
     let packagePath2 = path.resolve(managerPath, 'ui', 'package.json');
-    let str2 = await this.getStrPackage(packagePath2);
-    await this.setPackage(packagePath2, str2);
+    let [str, str2] = await Promise.all([this.getStrPackage(packagePath), this.getStrPackage(packagePath2)])
+      .catch(err => {
+        logger.error('read error', err);
+      });
+    await Promise.all([this.setPackage(packagePath, str), this.setPackage(packagePath2, str2)])
+      .catch(err => {
+        logger.error('write error', err);
+      });
     logger.info('job gitVersion task 2: }}')
   }
-  async setPackage (packagePath, str) {
-    let wStream = fs.createWriteStream(packagePath, {flags: 'r+'});
-    wStream.write(str, function (err) {
-      if (err) {
-        logger.error('write err', err);
-      }
-      wStream.close();
-    })
-    wStream.on('close', () => {
-    })
-    wStream.on('error', (err) => {
-      logger.error('write stream error', err)
+  async setPackage(packagePath, str) {
+    return new Promise((resolve, reject) => {
+      let wStream = fs.createWriteStream(packagePath, { flags: 'r+' });
+      wStream.write(str, function (err) {
+        if (err) {
+          logger.error('write err', err);
+          reject(err);
+        }
+        resolve();
+        wStream.close();
+      })
+      wStream.on('close', () => {
+      })
+      wStream.on('error', (err) => {
+        logger.error('write stream error', err)
+      })
     })
   }
-  async getStrPackage (packagePath) {
+  async getStrPackage(packagePath) {
     return new Promise((resolve, reject) => {
       const ins = this;
-      let rStream = fs.createReadStream(packagePath, {flags: 'r+'});
+      let rStream = fs.createReadStream(packagePath, { flags: 'r+' });
       let fc = '';
-      rStream.on('end', function() {
+      rStream.on('end', function () {
         let arr = fc.split(/\n/g);
-        for(let i in arr) {
+        for (let i in arr) {
           let str = arr[i];
           if (/"version":/.test(str)) {
             let vNo = str.replace(/.*(\d+\.\d+\.\d+).*/, '$1')
@@ -67,7 +75,7 @@ class GitVersionJob {
               arrVno.push(lastNoPlus);
               let strVno = arrVno.join('.');
               str = str.replace(vNo, strVno);
-              logger.info('job gitVersion new version number', {strVno});
+              logger.info('job gitVersion new version number', { strVno });
               ins.version = strVno;
             }
             arr.splice(i, 1, str);
@@ -77,20 +85,20 @@ class GitVersionJob {
         resolve(arr.join('\n'))
         rStream.close();
       })
-      rStream.on('data', function(data) {
+      rStream.on('data', function (data) {
         logger.debug('read stream data')
         fc += data;
       })
       rStream.on('close', function () {
         logger.debug('rstream close')
       })
-      rStream.on('error', function(err) {
+      rStream.on('error', function (err) {
         logger.error('job gitVersion rStream', err)
         reject(err)
       })
     })
   }
-  async autoCommit () {
+  async autoCommit() {
     logger.info('job gitVersion task begin')
     try {
       this.version = '';
